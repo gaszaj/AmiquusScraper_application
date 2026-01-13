3f47e434e372dfbd85ef9a196437555d74d7829d
Moses Adebayo <94234833+muzardemoses@users.noreply.github.com>
2026-01-13 03:12:47 +0100

    Load environment variables from .env file

--- server/db.ts
+++ server/db.ts
@@ -1,8 +1,9 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "@shared/schema";
 import pg from "pg";
+import dotenv from 'dotenv';
 
-
+dotenv.config();
 if (!process.env.DATABASE_URL) {
   throw new Error(
     "DATABASE_URL must be set. Did you forget to provision a database?",
