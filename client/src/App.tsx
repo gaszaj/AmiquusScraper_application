fcb588eba158748c03cd01a6955b7a70db15d277
Moses <muzardemoses@gmail.com>
2026-01-29 15:23:03 +0100

    Refactor referral handling logic in App component to improve clarity and maintainability

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -64,10 +64,13 @@ function App() {
     if (existing) {
       return;
     }
-  
+
     const referral =
-      window.affonso_referral ||
-      new URLSearchParams(window.location.search).get("via");
+    window.affonso_referral
+  
+    // const referral =
+    //   window.affonso_referral ||
+    //   new URLSearchParams(window.location.search).get("via");
   
     if (referral) {
       localStorage.setItem("affonso_referral", referral);
